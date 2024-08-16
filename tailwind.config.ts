682b013bcb8d59d0c33ef9ddfb25e58a807632a5
M Sufian Khan <sufiancoding@gmail.com>
2024-08-16 22:58:39 +0500

    tailwind config updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -149,6 +149,7 @@ const config = {
         },
       },
       animation: {
+        text: "text 5s ease infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         spotlight: "spotlight 2s ease .75s 1 forwards",
