15e555ce9b25eb95e810fc09cf23966fc327eb0c
M Sufian Khan <sufiancoding@gmail.com>
2024-08-16 22:59:55 +0500

    recent projects updated

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,9 +1,5 @@
 import { projects } from "@/data";
 import React from "react";
-// import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
-import Image from "next/image";
-import Link from "next/link";
-// import { PinContainer } from "./ui/3d-pin";
 import { FaLocationArrow } from "react-icons/fa6";
 import { Combined3DCard } from "./ui/3d-card";
 
@@ -20,92 +16,6 @@ const RecentProjects = () => {
             key={id}
             className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
-            {/* <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] sm:h-[40vh] mb-10">
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                  <img src="/bg.png" alt="bg-img" />
-                </div>
-                <img src={img} alt={title} className="z-10 absolute bottom-0" />
-              </div>
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {title}
-              </h1>
-              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
-                {des}
-              </p>
-
-              <div className="flex items-center justify-between mt-7 mb-3">
-                <div className="flex items-center">
-                  {iconLists.map((icon, index) => (
-                    <div
-                      key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
-                      }}
-                    >
-                      <img src={icon} alt="icon5" className="p-2" />
-                    </div>
-                  ))}
-                </div>
-
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
-                  </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
-              </div>
-            </PinContainer> */}
-
-            {/* <CardContainer className="flex flex-col sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] items-center justify-center sm:w-[570px] w-[80vw]">
-              <CardBody>
-                <CardItem translateZ="100" className="w-full mt-4">
-                  <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] sm:h-[40vh] mb-10">
-                    <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                      <img src="/bg.png" alt="bg-img" />
-                    </div>
-                    <img
-                      src={img}
-                      alt={title}
-                      className="z-10 absolute bottom-0"
-                    />
-                  </div>
-                </CardItem>
-                <CardItem
-                  translateZ="50"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
-                >
-                  Make things float in air
-                </CardItem>
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                >
-                  Hover over this card to unleash the power of CSS perspective
-                </CardItem>
-                <div className="flex justify-between items-center mt-20">
-                  <CardItem
-                    translateZ={20}
-                    as={Link}
-                    href="https://twitter.com/mannupaaji"
-                    target="__blank"
-                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                  >
-                    Try now →
-                  </CardItem>
-                  <CardItem
-                    translateZ={20}
-                    as="button"
-                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                  >
-                    Sign up
-                  </CardItem>
-                </div>
-              </CardBody>
-            </CardContainer> */}
-
             <Combined3DCard title={link} href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] sm:h-[40vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
