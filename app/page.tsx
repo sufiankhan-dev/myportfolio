c3be2bd39517a05efc0c7c8756f4f14c955cebc1
M Sufian Khan <sufiancoding@gmail.com>
2024-08-22 21:40:12 +0500

    footer added

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 import Approach from "@/components/Approach";
+import BuyMeCoffee from "@/components/BuyMeCoffee";
 import Client from "@/components/Client";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import { navItems } from "@/data";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+    <main className="dark:bg-black-100 relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        <Navbar />
+        {/* <BuyMeCoffee /> */}
         <Hero />
         <Grid />
         <RecentProjects />
-        <Client />
+        {/* <Client /> */}
+        <Skills />
         <Experience />
         <Approach />
         <Footer />
