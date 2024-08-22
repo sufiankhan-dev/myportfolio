d8493c3b1f5605e0a8b4207026908c605f9ab8dc
M Sufian Khan <sufiancoding@gmail.com>
2024-08-22 21:41:58 +0500

    section updated

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 
@@ -10,7 +9,7 @@ export default function Approach() {
       <h1 className="heading">
         My <span className="text-purple">approach</span>
       </h1>
-      <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-16 mx-auto px-8">
+      <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-y-10 md:gap-x-16 mx-auto md:px-8 select-none">
         <Card
           title="Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
@@ -20,7 +19,7 @@ export default function Approach() {
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
-            containerClassName="bg-emerald-900 rounded-3xl overflow-hidden"
+            containerClassName="bg-emerald-700 dark:bg-emerald-900 rounded-3xl overflow-hidden"
           />
         </Card>
         <Card
@@ -32,7 +31,7 @@ export default function Approach() {
         >
           <CanvasRevealEffect
             animationSpeed={3}
-            containerClassName="bg-pink-900 rounded-3xl overflow-hidden"
+            containerClassName="bg-pink-700 dark:bg-pink-900 rounded-3xl overflow-hidden"
             colors={[
               [255, 166, 158],
               [221, 255, 247],
@@ -49,7 +48,7 @@ export default function Approach() {
         >
           <CanvasRevealEffect
             animationSpeed={3}
-            containerClassName="bg-sky-600 rounded-3xl overflow-hidden"
+            containerClassName="bg-sky-500 dark:bg-sky-600 rounded-3xl overflow-hidden"
             colors={[[125, 211, 252]]}
           />
         </Card>
@@ -74,13 +73,8 @@ const Card = ({
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center
-       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl"
-      style={{
-        background: "rgb(4,7,29)",
-        backgroundColor:
-          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-      }}
+      className="border cursor-pointer border-gray-300 dark:border-gray-700 group/canvas-card flex items-center justify-center
+       max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl bg-gray-100 dark:bg-[#04071D]"
     >
       <AnimatePresence>
         {hovered && (
@@ -102,17 +96,16 @@ const Card = ({
           {icon}
         </div>
         <h2
-          className="dark:text-white text-center text-xl sm:text-3xl opacity-0 group-hover/canvas-card:opacity-100
-         relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white 
+          className="text-center text-xl sm:text-3xl opacity-0 group-hover/canvas-card:opacity-100
+         relative z-10 text-gray-900 dark:text-gray-100 mt-4 font-bold group-hover/canvas-card:text-white 
          group-hover/canvas-card:-translate-y-2 transition duration-200"
         >
           {title}
         </h2>
         <p
           className="text-sm opacity-0 group-hover/canvas-card:opacity-100
-         relative z-10 mt-4 group-hover/canvas-card:text-white text-center
+         relative z-10 mt-4 text-gray-200 dark:text-gray-300 text-center
          group-hover/canvas-card:-translate-y-2 transition duration-200"
-          style={{ color: "#E4ECFF" }}
         >
           {des}
         </p>
